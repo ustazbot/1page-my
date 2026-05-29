32e785e4e738daedca69e00069eac7107f0b333c
Bos <planetrizq@gmail.com>
2026-05-29 22:21:43 +0800

    feat: load all 5 template fonts in candidate layout (+ Bebas Neue for T2)

--- app/candidate/[subdomain]/layout.tsx
+++ app/candidate/[subdomain]/layout.tsx
@@ -1,24 +1,72 @@
 // app/candidate/[subdomain]/layout.tsx
-import { Playfair_Display, DM_Sans } from 'next/font/google'
+import {
+  Playfair_Display,
+  DM_Sans,
+  Source_Serif_4,
+  Lora,
+  Plus_Jakarta_Sans,
+  Syne,
+  Inter,
+  Bebas_Neue,
+} from 'next/font/google'
 
 const playfair = Playfair_Display({
   subsets: ['latin'],
   weight: ['700', '900'],
+  style: ['normal', 'italic'],
   variable: '--font-playfair',
   display: 'swap',
 })
-
 const dmSans = DM_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600'],
   variable: '--font-dm',
   display: 'swap',
 })
+const sourceSerif = Source_Serif_4({
+  subsets: ['latin'],
+  weight: ['300', '400'],
+  style: ['normal', 'italic'],
+  variable: '--font-source-serif',
+  display: 'swap',
+})
+const lora = Lora({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  style: ['normal', 'italic'],
+  variable: '--font-lora',
+  display: 'swap',
+})
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ['latin'],
+  weight: ['400', '600', '700', '800'],
+  variable: '--font-jakarta',
+  display: 'swap',
+})
+const syne = Syne({
+  subsets: ['latin'],
+  weight: ['700', '800'],
+  variable: '--font-syne',
+  display: 'swap',
+})
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+const bebas = Bebas_Neue({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-bebas',
+  display: 'swap',
+})
 
 export default function CandidateLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={`${playfair.variable} ${dmSans.variable}`}>
-      {children}
-    </div>
-  )
+  const fontVars = [
+    playfair.variable, dmSans.variable, sourceSerif.variable,
+    lora.variable, jakarta.variable, syne.variable, inter.variable,
+    bebas.variable,
+  ].join(' ')
+  return <div className={fontVars}>{children}</div>
 }
